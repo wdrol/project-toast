ffd18eeda4039c452eea58c256c03ab49b765a5b
Bill Drol <billdrol64@gmail.com>
2023-12-09 05:43:54 -0500

    Exercise 2a

--- src/components/ToastProvider/ToastProvider.js
+++ src/components/ToastProvider/ToastProvider.js
@@ -4,9 +4,17 @@ export const ToastContext = React.createContext();
 
 function ToastProvider({ children }) {
   const toastVariants = ["Notice", "Warning", "Success", "Error"];
+  const [toastList, setToastList] = React.useState([]);
 
   function addToastMessage(variant, message) {
-    alert(`Added ${variant}: ${message}`);
+    const newList = [...toastList];
+    newList.push({ id: Math.random(), variant, message });
+    setToastList(newList);
+  }
+
+  function removeToast(id) {
+    const newList = toastList.filter((toast) => toast.id != id);
+    setToastList(newList);
   }
 
   return (
@@ -16,7 +24,19 @@ function ToastProvider({ children }) {
         addToastMessage,
       }}
     >
-      {children}
+      <>
+        <div>
+          <ul>
+            {toastList.map((toast) => (
+              <li key={toast.id}>
+                {`${toast.variant}: ${toast.message}`}{" "}
+                <button onClick={() => removeToast(toast.id)}>X</button>
+              </li>
+            ))}
+          </ul>
+        </div>
+        {children}
+      </>
     </ToastContext.Provider>
   );
 }
